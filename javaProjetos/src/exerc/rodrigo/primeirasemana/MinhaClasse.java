4f72d7137f6144e9eb978e5fed669bb1018facb4
Rodrigo Martins <rodrigomartins.souza12345@gmail.com>
2025-03-05 12:50:20 -0300

    primeiro exercício java

--- javaProjetos/src/exerc/rodrigo/primeirasemana/MinhaClasse.java
+++ javaProjetos/src/exerc/rodrigo/primeirasemana/MinhaClasse.java
@@ -1,7 +1,12 @@
 package exerc.rodrigo.primeirasemana;
 public class MinhaClasse {
     
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws Exception {
+        String primeiroNome = "rodrigo";
+        String segundoroNome = "martins";
+        System.out.println(nomeCompleto(primeiroNome, segundoroNome));
+    }
+    public static String nomeCompleto(String primeiroNome,String segundoNome){
+        return primeiroNome.concat(" ").concat(segundoNome);
     }
 }
