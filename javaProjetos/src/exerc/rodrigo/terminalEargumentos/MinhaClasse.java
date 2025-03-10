3fea81ca0164b35e9a030744004e2be2f01b35e3
Rodrigo Martins <rodrigomartins.souza12345@gmail.com>
2025-03-10 18:49:58 -0300

    Testando terminal e Argumentos

---
+++ javaProjetos/src/exerc/rodrigo/terminalEargumentos/MinhaClasse.java
@@ -0,0 +1,7 @@
+package exerc.rodrigo.terminalEargumentos;
+
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Oi, fui executado pelo Terminal");
+    }
+}
